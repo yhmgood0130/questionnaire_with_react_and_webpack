a3ddd09dacc2c0a335feb4f3e15b93cdb5e71ec1
Hyunmo Yang <yhmgood0130@gmail.com>
2018-08-19 01:25:32 -0600

    Webpack config added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import './app.scss';
+
 class App extends Component {
     render() {
         return <p>Hello darkness smile old friend...</p>;
     }
 }
 
-render(<App />, document.getElementById("root"));
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,52 @@
-// module.exports = {
-//     entry: './src/app.js',
-//     output: {
-//         filename: './dist/app.bundle.js'
-//     }
-// }
-
 var webpack = require('webpack');
 var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.join(__dirname, 'dist');
 var APP_DIR = path.join(__dirname, 'src');
 
+const VENDOR_LIBS = [
+    'react', 'react-dom', 'react-router-dom'
+];
+
 var config = {
-    entry: APP_DIR + '/app.js',
+    entry: {
+        bundle: APP_DIR + '/Main.jsx',
+        vendor: VENDOR_LIBS
+    },
     output: {
         path: BUILD_DIR,
-        filename: 'app.bundle.js'
+        filename: '[name].js'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(js|jsx)$/,
-                // include: APP_DIR,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: 'file-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        })
+    ]
 }
 
 module.exports = config;
